6df21119351831ff8e577cdaa1b80cde78589394
Phillip Langley <philliplangley@champmbplangley.lan>
2022-05-05 13:58:10 -0500

    updating data policy to arrary count

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ resource "aws_ecr_repository" "this" {
 resource "aws_ecr_repository_policy" "ecr_policy" {
   count      = var.trusted_accounts != null ? 1 : 0
   repository = aws_ecr_repository.this.name
-  policy     = data.aws_iam_policy_document.resource_readonly_access.json
+  policy     = data.aws_iam_policy_document.resource_readonly_access[0].json
 }
 
 data "aws_iam_policy_document" "resource_readonly_access" {
