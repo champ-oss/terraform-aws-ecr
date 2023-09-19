af525ed2e0bbe992d2b98104893abb86e40a8eaf
Justin Reeves <reeves122@gmail.com>
2023-09-19 15:48:57 -0500

    remove terratest

--- examples/complete/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "registry_id" {
-  value       = [for s in module.this : s.registry_id[*]]
-  description = "repository id"
-}
-
-output "repository_url" {
-  value       = [for s in module.this : s.repository_url[*]]
-  description = "URL of first repository created"
-}
--- examples/complete/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "repositories" {
-  description = "list of the repos to test creating in aws ecr"
-  type        = list(string)
-  default     = []
-}
