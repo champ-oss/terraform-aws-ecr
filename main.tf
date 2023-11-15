8de3feaa83d5a1b617d2e8d59cc173847c56a515
Justin Reeves <reeves122@gmail.com>
2023-11-15 09:26:08 -0600

    terraform fmt

--- main.tf
+++ main.tf
@@ -93,7 +93,7 @@ data "aws_iam_policy_document" "resource_readonly_access" {
       effect = "Allow"
 
       principals {
-        type = "AWS"
+        type        = "AWS"
         identifiers = ["*"]
       }
 
